e6568776aa9385353ee549b290dc9c581476dca4
Benn King <bennkingy@gmail.com>
2019-03-02 16:55:19 +0000

    Bit of front end stuff no vue just yet :P

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -12,9 +12,18 @@
     <h1>Birdboard</h1>
     
     <ul>
-        @foreach ($projects as $project)
-            <li>{{ $project->title}}</li>
-        @endforeach
+        <!-- forelse is a foreach but returns empty if the array is empty -->
+        @forelse ($projects as $project)
+
+            <li> <!-- Loop through projects table and return the projects in list items -->
+                <a href="{{ $project->path() }}">{{ $project->title }}</a>
+            </li>
+
+        @empty
+
+            <li>No Projects yet.</li>
+
+        @endforelse
     </ul>
 
 </body>
