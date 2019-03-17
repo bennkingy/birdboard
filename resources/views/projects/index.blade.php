162d5155dd3a455727343fe0e0247390122a029d
Benn King <bennkingy@gmail.com>
2019-03-17 14:00:01 +0000

    Front end changes

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,27 +4,36 @@
 @extends ('layouts.app')
 @section('content')
 
-    <div class="flex items-center mb-3">
+    <header class="flex items-center mb-3 py-4">
 
-        <h1 class="mr-auto">Birdboard</h1>
+        <div class="flex justify-between items-center w-full">
 
-        <a href="/projects/create">Create project</a>
-    
-    </div>
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
 
-    <div class="flex flex-wrap -mx-3">
+            <a href="/projects/create" class="button bg-blue">Create project</a>
+
+        </div>
+
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
 
         @forelse ($projects as $project)
 
-        <div class="w-1/3 px-3 pb-6">
+        <div class="lg:w-1/3 px-3 pb-6">
 
-            <div class="bg-white rounded shadow p-5" style="height:200px;box-sizing: content-box;">
+            <div class="bg-white rounded-lg shadow p-5" style="height:200px;box-sizing: content-box;">
                     
-                <h3 class="font-normal text-xl mb-6 py-4">{{ $project->title }}</h3>
+                <h3 class="font-normal text-xl mb-6 py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                    
+                    <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                
+                </h3>
 
                 <div class="text-grey">{{ str_limit($project->description, 70) }}</div>
 
             </div>
+
         </div>
 
         @empty
@@ -33,9 +42,11 @@
 
         @endforelse
 
-    </div>
+    </main>
 
-    {{-- <ul>
+    {{--
+        
+    <ul>
         <!-- forelse is a foreach but returns empty if the array is empty -->
         @forelse ($projects as $project)
 
@@ -48,6 +59,8 @@
             <li>No Projects yet.</li>
 
         @endforelse
-    </ul> --}}
+    </ul>
+    
+    --}}
 
 @endsection
